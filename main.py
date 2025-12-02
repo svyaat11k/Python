e67c1c942decd640dd5010718431f082c1688ea5
Sviatoslav <svyatikkushkevich632@gmail.com>
2025-12-02 14:04:54 +0100

    hhh

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 print("Hello World!")
 print("Hello World!")
 print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+
